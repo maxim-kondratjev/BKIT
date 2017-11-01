fb63267df94a5f24376ad01e66fbc42cf46a0472
maximusman533 <32134035+maximusman533@users.noreply.github.com>
2017-11-01 16:04:22 +0300

    Add files via upload

---
+++ Лаб №3/Лаб№3/MYClasses.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+abstract class Figure : IPrint, IComparable
+{
+    abstract public double find_square();
+
+    public void Print()
+    {
+        System.Console.WriteLine(this.ToString());
+    }
+
+    public int CompareTo(Object rhs)
+    {
+        Figure obj = rhs as Figure;
+        if (this.find_square() - obj.find_square() > 0.0001)
+        {
+            if (this.find_square() < obj.find_square())
+            {
+                return -1;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+        else
+        {
+            return 0;
+        }
+
+    }
+}
+
+class Rectangle : Figure, IPrint
+{
+    public double width { get; set; }
+    public double heigth { get; set; }
+
+
+    public Rectangle(double width1, double heigth1)
+    {
+        heigth = heigth1; width = width1;
+    }
+
+    public override double find_square()
+    {
+        return heigth * width;
+    }
+
+    public override string ToString() => "Прямоугольник "+width.ToString() + " на " + heigth.ToString();
+    
+}
+
+class Square : Rectangle, IPrint
+{
+    public Square(double heigth1) : base(heigth1, heigth1)
+    { }
+
+    public override string ToString() => "Квадрат стороной " + heigth.ToString();
+}
+
+class Circle : Figure, IPrint
+{
+    public double radius { get; set; }
+
+    public Circle(double radius1)
+    {
+        radius = radius1;
+    }
+
+    public override double find_square()
+    {
+        return (Math.PI * Math.Pow(radius, 2));
+    }
+
+    public override string ToString() => "Круг радиусом " + radius.ToString();
+}
+
+interface IPrint
+{
+    void Print();
+}
+
+class SimpleStack<T> : SimpleList.SimpleList<T> where T : IComparable
+{
+
+
+    public void Push(T element)
+    {
+        Add(element);
+    }
+
+    public T Pop()
+    {
+
+        SimpleList.SimpleListItem<T> itemPopped = last;
+        Count = Count - 1;
+        if (Count == 0)
+        {
+            last = null;
+            first = null;
+        }
+        else
+        {
+            SimpleList.SimpleListItem<T> newLastItem = this.GetItem(Count - 1);
+            newLastItem.next = null;
+            last = newLastItem;
+        }
+        return itemPopped.data;
+
+    }
+}
+
+
---
+++ Лаб №3/Лаб№3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Лаб3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle(10, 20);
+            Square sqr = new Square(12);
+            Circle circle = new Circle(4);
+            //------------------------------------------------------------
+            ArrayList list = new ArrayList();
+            list.Add(rect); list.Add(sqr); list.Add(circle);
+            list.Sort();
+            foreach (Figure obj in list)
+            {
+                System.Console.WriteLine(obj.GetType().Name);
+                obj.Print();
+            }
+            Console.WriteLine("-------------------------------------------");
+            List<Figure> list2 = new List<Figure>();
+            list2.Add(rect); list2.Add(sqr); list2.Add(circle);
+            list2.Sort();
+            foreach (Figure obj in list2)
+            {
+                System.Console.WriteLine(obj.GetType().Name);
+                obj.Print();
+            }
+            Console.WriteLine("-------------------------------------------");
+            SparseMatrix.Matrix<Figure> drop = new SparseMatrix.Matrix<Figure>(2, 2, 1, new Circle(0));
+            drop[0, 0, 0] = new Circle(1);
+            drop[1, 0, 0] = new Square(2);
+            drop[1, 1, 0] = new Rectangle(1, 2);
+            System.Console.WriteLine(drop.ToString());
+            Console.WriteLine("-------------------------------------------");
+            SimpleStack<Figure> primer = new SimpleStack<Figure>();
+            primer.Add(circle); primer.Add(rect); primer.Add(sqr);
+            primer.Sort();
+
+            for (int i = primer.Count; i > 0; i--)
+            {
+                System.Console.WriteLine(primer.Pop());
+            }
+            System.Console.ReadLine();
+        }
+    }
+}
